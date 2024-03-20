a65fc1f46f5afa793a25e1da2d6257cc017da6ab
Terry Holmes <tchaser3@outlook.com>
2024-03-20 07:11:37 -0400

    Add project files.

---
+++ BJCDLLTesting/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace BJCDLLTesting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.cboVehicleID = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmployeeID = new System.Windows.Forms.TextBox();
+            this.txtBJCNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(305, 274);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(119, 50);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.Location = new System.Drawing.Point(305, 218);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(119, 50);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // cboVehicleID
+            // 
+            this.cboVehicleID.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboVehicleID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboVehicleID.FormattingEnabled = true;
+            this.cboVehicleID.Location = new System.Drawing.Point(171, 79);
+            this.cboVehicleID.Name = "cboVehicleID";
+            this.cboVehicleID.Size = new System.Drawing.Size(121, 24);
+            this.cboVehicleID.TabIndex = 2;
+            this.cboVehicleID.SelectedIndexChanged += new System.EventHandler(this.cboVehicleID_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Select Vehicle ID";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtEmployeeID
+            // 
+            this.txtEmployeeID.Location = new System.Drawing.Point(171, 110);
+            this.txtEmployeeID.Name = "txtEmployeeID";
+            this.txtEmployeeID.Size = new System.Drawing.Size(121, 20);
+            this.txtEmployeeID.TabIndex = 4;
+            // 
+            // txtBJCNumber
+            // 
+            this.txtBJCNumber.Location = new System.Drawing.Point(171, 136);
+            this.txtBJCNumber.Name = "txtBJCNumber";
+            this.txtBJCNumber.ReadOnly = true;
+            this.txtBJCNumber.Size = new System.Drawing.Size(121, 20);
+            this.txtBJCNumber.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 23);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Employee ID";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 23);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Vehicle ID";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 354);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBJCNumber);
+            this.Controls.Add(this.txtEmployeeID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboVehicleID);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnClose);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.ComboBox cboVehicleID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmployeeID;
+        private System.Windows.Forms.TextBox txtBJCNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ BJCDLLTesting/Form1.cs
@@ -0,0 +1,161 @@
+﻿/* Title:           BJC Dll Testing
+ * Date:            4-25-16
+ * Author:          Terry Holmles
+ *
+ * Description:     This form is for testing */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VehiclesDLL;
+using VehicleHistoryDLL;
+
+namespace BJCDLLTesting
+{
+    public partial class Form1 : Form
+    {
+        //setting up the classes
+        MessagesClass TheMessagesClass = new MessagesClass();
+        VehicleClass TheVehicleClass = new VehicleClass();
+        VehicleHistoryClass TheVehicleHistoryClass = new VehicleHistoryClass();
+
+        //setting up the data
+        VehiclesDataSet TheVehiclesDataSet;
+        VehicleHistoryDataSet TheVehicleHistoryDataSet;
+
+        struct VehicleStructure
+        {
+            public int mintVehicleID;
+            public int mintBJCNumber;
+            public string mstrMake;
+            public string mstrModel;
+            public int mintEmployeeID;
+        }
+
+        VehicleStructure[] TheVehicles;
+        int mintVehicleCounter;
+        int mintVehicleUpperLimit;
+
+        string mstrErrorMessage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            //this will close the program
+            TheMessagesClass.CloseTheProgram();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bool blnFatalError = false;
+
+            blnFatalError = LoadVehicleStructure();
+
+            if(blnFatalError == true)
+            {
+                TheMessagesClass.ErrorMessage("Mother Fucker, there is a problem");
+            }
+        }
+        private bool LoadVehicleStructure()
+        {
+            bool blnFatalError = false;
+            int intCounter;
+            int intNumberOfRecords;
+
+            try
+            {
+                TheVehiclesDataSet = TheVehicleClass.GetVehiclesInfo();
+
+                intNumberOfRecords = TheVehiclesDataSet.vehicles.Rows.Count - 1;
+                TheVehicles = new VehicleStructure[intNumberOfRecords + 1];
+                mintVehicleCounter = 0;
+                cboVehicleID.Items.Add("SELECT");
+
+                for(intCounter = 0; intCounter <= intNumberOfRecords; intCounter++)
+                {
+                    TheVehicles[mintVehicleCounter].mintVehicleID = Convert.ToInt32(TheVehiclesDataSet.vehicles.Rows[intCounter][0]);
+                    TheVehicles[mintVehicleCounter].mintBJCNumber = Convert.ToInt32(TheVehiclesDataSet.vehicles.Rows[intCounter][1]);
+                    TheVehicles[mintVehicleCounter].mstrMake = Convert.ToString(TheVehiclesDataSet.vehicles.Rows[intCounter][4]).ToUpper();
+                    TheVehicles[mintVehicleCounter].mstrModel = Convert.ToString(TheVehiclesDataSet.vehicles.Rows[intCounter][5]).ToUpper();
+                    TheVehicles[mintVehicleCounter].mintEmployeeID = Convert.ToInt32(TheVehiclesDataSet.vehicles.Rows[intCounter][11]);
+                    cboVehicleID.Items.Add(Convert.ToString(TheVehicles[mintVehicleCounter].mintVehicleID));
+                    mintVehicleUpperLimit = mintVehicleCounter;
+                    mintVehicleCounter++;
+                }
+
+                mintVehicleCounter = 0;
+                cboVehicleID.SelectedIndex = 0;
+            }
+            catch(Exception Ex)
+            {
+                mstrErrorMessage = Ex.Message;
+
+                blnFatalError = true;
+            }
+
+            return blnFatalError;
+        }
+
+        private void cboVehicleID_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int intCounter;
+            int intVehicleIDForSearch;
+
+            if(cboVehicleID.Text != "SELECT")
+            {
+                intVehicleIDForSearch = Convert.ToInt32(cboVehicleID.Text);
+
+                for(intCounter = 0; intCounter <= mintVehicleUpperLimit; intCounter++)
+                {
+                    if(intVehicleIDForSearch == TheVehicles[intCounter].mintVehicleID)
+                    {
+                        txtBJCNumber.Text = Convert.ToString(TheVehicles[intCounter].mintBJCNumber);
+                        txtEmployeeID.Text = Convert.ToString(TheVehicles[intCounter].mintEmployeeID);
+                    }
+                }
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            //setting local variables
+            int intCounter;
+            int intVehicleIDForSearch;
+            string strInformation = "";
+
+            try
+            {
+                intVehicleIDForSearch = Convert.ToInt32(cboVehicleID.Text);
+
+                //loop
+                for(intCounter = 0; intCounter <= mintVehicleUpperLimit; intCounter++)
+                {
+                    if (intVehicleIDForSearch == TheVehicles[intCounter].mintVehicleID)
+                    {
+                        TheVehiclesDataSet.vehicles.Rows[intCounter][11] = txtEmployeeID.Text;
+                        TheVehicleClass.UpdateVehiclesDB(TheVehiclesDataSet);
+                        strInformation = TheVehicleHistoryClass.CreateVehicleHistoryTransaction(intVehicleIDForSearch, 1211, 12, 12, "WHY ME", "FUCK");
+
+                    }
+
+                }
+
+                TheMessagesClass.InformationMessage("It Is Fucking Done and " + strInformation);
+            }
+            catch(Exception Ex)
+            {
+                TheMessagesClass.ErrorMessage(Ex.Message);
+            }
+        }
+    }
+}
